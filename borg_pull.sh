7b7563264b095ff7be59be5925d4926e44428986
Patrick <patrick@snuggleton.net>
2023-02-05 16:34:51 -0800

    Cleaned up a lot of string formatting

--- borg_pull.sh
+++ borg_pull.sh
@@ -1,87 +1,108 @@
 #!/bin/bash
 
 # This script requires SSHFS and Borg.
-# Back up data from a remote location to a local repository.
+# It backs up data from a remote location to a local repository.
 
 # =============
 # CONFIGURATION
 # =============
 
-DATA_HOST="example-hostname"
-DATA_PATHS=( "/example_location_1" \
-    "/example_location_2" )
+# Hostname, domain name, or IP address of the machine where the data to be backed up resides.
+DATA_HOST=""
+# White-space-delimited list of absolute paths of directories on the remote machine to be backed up.
+DATA_PATHS=( "" )
 
-SSHFS_MOUNTPOINT="/where-the-remote-system-will-be-mounted"
-SSHFS_USER="your-username"
-SSHFS_PORT="22"
+# Absolute local path where the remote system will be temporarily mounted locally.
+# The directory will be created. Pick someplace to which you have write access.
+SSHFS_MOUNTPOINT=""
+# The username for the account on the remote machine as which we’ll be loggin in via SSH.
+SSHFS_USER=""
+SSHFS_PORT="8022"
 
-BACKUP_ROOT="/where-you-will-keep-your-backups"
-BACKUP_DATE=`date +"%Y-%m-%d_%H:%M"`
+# The absolute local path for the directory where the backups will be stored.
+BACKUP_ROOT="/mnt/butter/backups"
+# A command to generate the date stamp that will appear in a backup’s title.
+BACKUP_DATE=$(date +"%Y-%m-%d_%H:%M")
 
+# Whether or not to compacts repositories after backing up. I haven’t tested this
+# because I have an older version of Borg that doesn’t support compacting.
 COMPACT_REPOS="false"
+
 DAILIES_TO_RETAIN="7"
 WEEKLIES_TO_RETAIN="4"
 
+# This script is intended to be run unattended. If this option is set to “true”,
+# the script will pause for a few seconds before exiting if everything is successful,
+# and wait for user input before exiting if anything goes wrong.
 LINGER_ON_EXIT="true"
+
+EMPHASIS_COLOR="\e[36m"
+ERROR_COLOR="\e[91m"
+SUCCESS_COLOR="\e[32m"
+PROMPT_COLOR="\e[30;106m"
 
 # =============
 # Mise en place
 # =============
 
+NO_COLOR="\e[0m"
 ERRORLEVEL=0
 SUCCESSES="false"
-FAILURES="false"
+PROBLEMS="false"
 declare -A STATA
 
 _backup() {
     DATA_PATH=$1
-    FULL_REPO_PATH="$BACKUP_ROOT"/"$DATA_HOST""$DATA_PATH"
-    printf "Backing up $DATA_HOST$DATA_PATH to $BACKUP_ROOT$DATA_PATH\n"
-    borg create --stats --list "$FULL_REPO_PATH"::"$BACKUP_DATE" "$SSHFS_MOUNTPOINT""$DATA_PATH"
-    if [[ $? -eq 0 ]]
-        then printf "Success.\n\n"
+    FULL_REPO_PATH="${BACKUP_ROOT}/${DATA_HOST}${DATA_PATH}"
+    printf "Backing up ${EMPHASIS_COLOR}%s%s${NO_COLOR}\nto %s%s...\n" "$DATA_HOST" "$DATA_PATH" "$BACKUP_ROOT" "$DATA_PATH"
+    if borg create --stats --progress --exclude /*/lost+found "$FULL_REPO_PATH"::"$BACKUP_DATE" "$SSHFS_MOUNTPOINT""$DATA_PATH"
+        then printf "%bSuccess.%b\n\n" "$SUCCESS_COLOR" "$NO_COLOR"
         STATA["${DATA_PATH}"]="SUCCESS"
         SUCCESSES="true"
         if [[ $COMPACT_REPOS == "true" ]]
             then borg --progress compact "$FULL_REPO_PATH"
         fi
-        printf "Pruning $FULL_REPO_PATH...\n"
-        borg prune --list --keep-daily="$DAILIES_TO_RETAIN" --keep-weekly="$WEEKLIES_TO_RETAIN" "$FULL_REPO_PATH"
-        if [[ ! $? -eq 0 ]]
+        printf "Pruning %s...\n" "$FULL_REPO_PATH"
+        if ! borg prune --keep-daily="$DAILIES_TO_RETAIN" --keep-weekly="$WEEKLIES_TO_RETAIN" "$FULL_REPO_PATH"
             then ERRORLEVEL=1
-            FAILURES="true"
+            PROBLEMS="true"
         fi
         printf "\n"
+
         else STATA["${DATA_PATH}"]="$?"
         ERRORLEVEL=1
-        FAILURES="true"
+        PROBLEMS="true"
         printf "\n"
     fi
 }
 
 _delete_mountpoint() {
     if [[ ! "$(ls -A $SSHFS_MOUNTPOINT)" ]]
-        then printf "Deleting mountpoint $SSHFS_MOUNTPOINT...\n"
-        rmdir "$SSHFS_MOUNTPOINT"
-        if [[ $? -eq 0 ]]
+        then printf "Deleting mountpoint %s...\n" "$SSHFS_MOUNTPOINT"
+        if rmdir "$SSHFS_MOUNTPOINT"
             then printf "Deleted.\n\n"
-            else printf "Failed to delete the mountpoint.\n\n"
+            else printf "%bFailed to delete the mountpoint.%b\n\n" "$ERROR_COLOR" "$NO_COLOR"
             ERRORLEVEL=1
         fi
-        else printf "Not deleting the mountpoint at $SSHFS_MOUNTPOINT\nbecause it looks like there’s still something in it. Weird.\n\n"
+        else printf "%bNot deleting the mountpoint at %s\nbecause it looks like there’s still something in it. Weird.%b\n\n" "$ERROR_COLOR" "$SSHFS_MOUNTPOINT" "$NO_COLOR"
         ERRORLEVEL=1
     fi
 }
 
 _exit() {
     if [[ $ERRORLEVEL -eq 0 ]]
-        then if [[ $LINGER_ON_EXIT -eq "true" ]]
-            then printf "This message will self-destruct.\n\n"
+        then if [[ $LINGER_ON_EXIT == "true" ]]
+            then printf "%bThis message will self-destruct.%b\n\n" "$PROMPT_COLOR" "$NO_COLOR"
             sleep 5
-        fi
         exit 0
-        else if [[ $LINGER_ON_EXIT == "true" ]]
-            then read -n1 -r -p "Press any key to dismiss."
+        fi
+        elif [[ $LINGER_ON_EXIT == "true" ]]
+            then printf "%b" "$PROMPT_COLOR"
+            read -n1 -r -p "Press Enter to dismiss." input
+            printf "%b\n" "$NO_COLOR"
+            if [[ $input = "" ]]
+            then exit 1
+            else _exit
         fi
         exit 1
     fi
@@ -91,29 +112,27 @@ _exit() {
 # Prep
 # ====
 
-printf "\nWe’re about to back up the following remote directories from $DATA_HOST:\n"
+printf "\nWe’re about to back up the following remote directories from %b%s%b:\n" "$EMPHASIS_COLOR" "${DATA_HOST}" "$NO_COLOR"
 for i in "${DATA_PATHS[@]}"
 do
-    printf "• $i\n"
+    printf "• %b%s%b\n" "$EMPHASIS_COLOR" "${i}" "$NO_COLOR"
 done
 printf "\n"
 
 if [[ ! -d "$SSHFS_MOUNTPOINT" ]]
-    then printf "Creating mountpoint $SSHFS_MOUNTPOINT...\n"
-    mkdir "$SSHFS_MOUNTPOINT"
-    if [[ ! $? -eq 0 ]]
-        then printf "We seem to be having trouble creating the SSHFS mountpoint. Aborting!\n\n"
+    then printf "Creating mountpoint %s...\n" "$SSHFS_MOUNTPOINT"
+    if mkdir "$SSHFS_MOUNTPOINT"
+        then printf "Success.\n\n"
+        else printf "%bWe seem to be having trouble creating the SSHFS mountpoint. Aborting!%b\n\n" "$ERROR_COLOR" "$NO_COLOR"
         ERRORLEVEL=1
         _exit
-        else printf "Success.\n\n"
     fi
 fi
 
-printf "Mounting $DATA_HOST at $SSHFS_MOUNTPOINT...\n"
-sshfs -o ro,port="$SSHFS_PORT" "$SSHFS_USER"@"$DATA_HOST":/ "$SSHFS_MOUNTPOINT"
-if [[ $? -eq 0 ]]
+printf "Mounting %s at %s...\n" "$DATA_HOST" "$SSHFS_MOUNTPOINT"
+if sshfs -o ro,port="$SSHFS_PORT" "$SSHFS_USER"@"$DATA_HOST":/ "$SSHFS_MOUNTPOINT"
     then printf "Success.\n\n"
-    else printf "We seem to be having trouble mounting the remote remote file system.\n\n"
+    else printf "%bWe seem to be having trouble mounting the remote remote file system.%b\n\n" "$ERROR_COLOR" "$NO_COLOR"
     ERRORLEVEL=1
     _delete_mountpoint
     _exit
@@ -125,40 +144,39 @@ fi
 
 for i in "${DATA_PATHS[@]}"
 do
-    _backup $i
+    _backup "$i"
 done
 
 # ==========
 # Denouement
 # ==========
 
-printf "Unmounting $DATA_HOST from $SSHFS_MOUNTPOINT...\n"
-fusermount -u "$SSHFS_MOUNTPOINT"
-if [[ $? -eq 0 ]]
+printf "Unmounting %s from %s...\n" "$DATA_HOST" "$SSHFS_MOUNTPOINT"
+if fusermount -u "$SSHFS_MOUNTPOINT"
     then printf "Success.\n\n"
     _delete_mountpoint
-    else printf "We’re having trouble unmounting the remote file system at $SSHFS_MOUNTPOINT.\n"
-    printf "You’ll probably want to take a look at it.\n\n"
+    else printf "%bWe’re having trouble unmounting the remote file system at %s.\n" "$ERROR_COLOR" "$SSHFS_MOUNTPOINT"
+    printf "You’ll probably want to have a look at it.%b\n\n" "$NO_COLOR"
     ERRORLEVEL=1
 fi
 
-printf "==========\nCONCLUSION\n==========\n\n"
+printf "%b==========\nCONCLUSION\n==========%b\n\n" "${EMPHASIS_COLOR}" "$NO_COLOR"
 if [[ $SUCCESSES == "true" ]]
     then printf "The following locations backed up successfully:\n"
     for i in "${DATA_PATHS[@]}"
     do
-        if [[ ${STATA[${i}]} -eq "SUCCESS" ]]
-            then printf "• $i\n"
+        if [[ ${STATA[${i}]} == "SUCCESS" ]]
+            then printf "• %b%s%b\n" "$EMPHASIS_COLOR" "$i" "$NO_COLOR"
         fi
     done
     printf "\n"
 fi
-if [[ $FAILURES == "true" ]]
-    then printf "The following locations experienced errors:\n"
+if [[ $PROBLEMS == "true" ]]
+    then printf "The following locations experienced issues:\n"
     for i in "${DATA_PATHS[@]}"
     do
-        if [[ ! ${STATA[${i}]} -eq "SUCCESS" ]]
-            then printf "\e[31m• $i\n"
+        if [[ ! ${STATA[${i}]} == "SUCCESS" ]]
+            then printf "• %b%s%b\n" "$ERROR_COLOR" "$i" "$NO_COLOR"
         fi
     done
     printf "\n"
